6707b0226350bc1b4c105fc127be3a23af86dfa3
Prethush <ktmirash49@.gmail.com>
2022-08-19 23:39:32 +0530

    product is added to local storage and product validation completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ProductAddForm from "./Components/ProductAddForm";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div>
       <ProductAddForm />
+      <ToastContainer />
     </div>
   );
 }
--- src/Components/ProductAddForm.js
+++ src/Components/ProductAddForm.js
@@ -1,6 +1,63 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+
+let initialState = {
+  name: "",
+  image: "",
+  desc: "",
+  price: "",
+  category: "",
+  ratings: "",
+};
+const IMG_REGEX = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/;
 
 function ProductAddForm() {
+  // form fields
+  const [formVal, setFormVal] = useState(initialState);
+  // product array to store all the product details
+  const [prodArr, setProdArr] = useState([]);
+  const [validImgUrl, setValidImgUrl] = useState(false);
+  const [validRatings, setValidRatings] = useState(false);
+  let { name, image, desc, price, category, ratings } = formVal;
+
+  useEffect(() => {
+    //checking if a productArr exist in localstorage
+    const products = JSON.parse(localStorage.getItem("prodArr")) || [];
+    setProdArr(products);
+  }, []);
+
+  // storing the values of form fields to state and checking some validation
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    if (name === "name") {
+      value = value.trim();
+    }
+    if (name === "image") {
+      const imgResult = IMG_REGEX.test(value);
+      if (!imgResult) {
+        toast.error("Invalid image file");
+      }
+      setValidImgUrl(imgResult);
+    }
+
+    if (name === "ratings") {
+      const ratingsResult = value <= 10;
+      if (ratingsResult) {
+        toast.error("Ratings should be less than or equal to 10");
+      }
+      setValidRatings(ratingsResult);
+    }
+    setFormVal({ ...formVal, [name]: value });
+  };
+
+  // add product to the local storage
+  const addProduct = (e) => {
+    e.preventDefault();
+    prodArr.push(formVal);
+    localStorage.setItem("prodArr", JSON.stringify(prodArr));
+    setFormVal(initialState);
+    toast.success(`${name} is added`);
+  };
   return (
     <section className="vh-100 bg-image">
       <div className="mask d-flex align-items-center h-100 gradient-custom-3">
@@ -13,7 +70,7 @@ function ProductAddForm() {
                     Add Product
                   </h2>
 
-                  <form>
+                  <form onSubmit={addProduct}>
                     <div className="form-outline mb-4">
                       <label className="form-label" htmlFor="form3Example1cg">
                         Product Name
@@ -23,6 +80,8 @@ function ProductAddForm() {
                         id="form3Example1cg"
                         className="form-control form-control-lg"
                         name="name"
+                        value={name}
+                        onChange={handleChange}
                       />
                     </div>
 
@@ -34,6 +93,9 @@ function ProductAddForm() {
                         type="file"
                         id="form3Example2cg"
                         className="form-control form-control-lg"
+                        name="image"
+                        value={image}
+                        onChange={handleChange}
                       />
                     </div>
                     <div className="mb-3">
@@ -47,6 +109,9 @@ function ProductAddForm() {
                         className="form-control"
                         id="exampleFormControlTextarea1"
                         rows="3"
+                        name="desc"
+                        value={desc}
+                        onChange={handleChange}
                       ></textarea>
                     </div>
                     <div className="form-outline mb-4">
@@ -58,6 +123,8 @@ function ProductAddForm() {
                         id="form3Example3cg"
                         className="form-control form-control-lg"
                         name="price"
+                        value={price}
+                        onChange={handleChange}
                       />
                     </div>
 
@@ -69,11 +136,14 @@ function ProductAddForm() {
                         className="form-select"
                         aria-label="Default select example"
                         id="form3Example4cdg"
+                        name="category"
+                        onChange={handleChange}
                       >
-                        <option defaultValue="addd">Select Category</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option defaultValue="">Select Category</option>
+                        <option value="book">Book</option>
+                        <option value="mobile">Mobile</option>
+                        <option value="shoe">Shoe</option>
+                        <option value="laptop">Laptop</option>
                       </select>
                     </div>
                     <div className="form-outline mb-4">
@@ -85,16 +155,26 @@ function ProductAddForm() {
                         id="form3Example5cg"
                         className="form-control form-control-lg"
                         name="ratings"
+                        onChange={handleChange}
+                        value={ratings}
                       />
                     </div>
-                    <div className="d-flex justify-content-center">
-                      <button
-                        type="button"
-                        className="btn btn-primary text-white btn-block btn-lg gradient-custom-4 form-control"
-                      >
-                        Add Product
-                      </button>
-                    </div>
+
+                    <input
+                      type="submit"
+                      className="btn btn-primary text-white btn-block btn-lg gradient-custom-4 form-control"
+                      value="Add Product"
+                      disabled={
+                        !name ||
+                        !image ||
+                        !desc ||
+                        !price ||
+                        !category ||
+                        !ratings ||
+                        !validImgUrl ||
+                        !validRatings
+                      }
+                    />
                   </form>
                 </div>
               </div>
